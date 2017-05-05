ef742bdacc71826e771083034c79ef74d048ad2e
Chris Ketron <caketron@gmail.com>
2017-05-04 21:03:27 -0500

    add conf.py

---
+++ conf.py
@@ -0,0 +1,8 @@
+# -- General configuration ----------------------------
+project = 'Lake Afton Public Observatory Presenter Notes'
+copyright = '&copy; 20175 Lake Afton Public Observatory'
+
+version = '2017.05.04.0'
+
+# -- Options for HTML output --------------------------
+html_theme = 'cyborg'
