e7d0cb5828389b8a73a1a5d23d1b63ea1129d7fc
Chris Ketron <>
2018-09-29 17:03:51 -0500

    update version

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@
 project = 'Lake Afton Public Observatory Presenter Notes'
 copyright = '&copy; 2018 Lake Afton Public Observatory'
 
-version = '2018.05.12.0'
+version = '2018.09.29.1'
 
 # -- Options for HTML output --------------------------
 html_theme = 'cyborg'
